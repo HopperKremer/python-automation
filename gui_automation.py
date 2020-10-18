2d405984b08ebd01f2d22d39ca64bd3c7511546b
Hopper <hopperkremer@gmail.com>
2020-10-18 13:06:14 -0400

    i can haz green square?

--- gui_automation.py
+++ gui_automation.py
@@ -1,2 +1,14 @@
 import pyautogui
-print('hi')
+
+def cornerCircle():
+    for i in range(10):
+        pyautogui.moveTo(100, 100, duration=0.25)
+        pyautogui.moveTo(200, 100, duration=0.25)
+        pyautogui.moveTo(200, 200, duration=0.25)
+        pyautogui.moveTo(100, 200, duration=0.25)
+def relativeCircle():
+    for i in range(10):
+        pyautogui.moveRel(100, 0, duration=0.25)
+        pyautogui.moveRel(0, 100, duration=0.25)
+        pyautogui.moveRel(-100, 0, duration=0.25)
+        pyautogui.moveRel(0, -100, duration=0.25)
